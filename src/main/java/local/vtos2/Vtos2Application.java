5c90ba8018b466f13b7470e9bb71813b77c76a03
Baliasnyi Serghii <sergey070373@gmail.com>
2022-11-19 17:36:52 +0000

    Init

---
+++ src/main/java/local/vtos2/Vtos2Application.java
@@ -0,0 +1,13 @@
+package local.vtos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vtos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vtos2Application.class, args);
+	}
+
+}
---
+++ src/test/java/local/vtos2/Vtos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package local.vtos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vtos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
