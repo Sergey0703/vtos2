d0867c20eedb5fa6b4a8c3a05cb40a9517d76d37
Baliasnyi Serghii <sergey070373@gmail.com>
2022-12-01 22:27:01 +0000

    securityOk3

--- src/main/java/local/vtos2/config/SecurityConfig.java
+++ src/main/java/local/vtos2/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import local.vtos2.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,8 +15,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     // private final AuthProviderImpl authProvider;
 
-
-
 //    @Autowired
 //    public SecurityConfig(AuthProviderImpl authProvider) {
 //        this.authProvider = authProvider;
@@ -25,8 +24,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService){
         this.personDetailsService=personDetailsService;
-
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+                    http.csrf().disable()
+                        .authorizeRequests()
+                        .antMatchers("/auth/login","/error").permitAll()
+                        .anyRequest().authenticated()
+                        .and()
+                        .formLogin().loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/",true)
+                        .failureUrl("/auth/login?error");
+    }
+
+
+    //tweak authentication
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(personDetailsService);
     }
