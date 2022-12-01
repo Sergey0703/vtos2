500bbac165f57ef569df30da6988044afc11f6f5
Baliasnyi Serghii <sergey070373@gmail.com>
2022-12-01 20:26:11 +0000

    securityOk2

--- src/main/java/local/vtos2/config/SecurityConfig.java
+++ src/main/java/local/vtos2/config/SecurityConfig.java
@@ -1,22 +1,38 @@
 package local.vtos2.config;
 
-import local.vtos2.security.AuthProviderImpl;
+import local.vtos2.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    // private final AuthProviderImpl authProvider;
 
+
+
+//    @Autowired
+//    public SecurityConfig(AuthProviderImpl authProvider) {
+//        this.authProvider = authProvider;
+//    }
+
+    private final PersonDetailsService personDetailsService;
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService){
+        this.personDetailsService=personDetailsService;
+
+    }
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(personDetailsService);
     }
 
-    protected void configure(AuthenticationManagerBuilder auth){
-    auth.authenticationProvider(authProvider);
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
