c239eca689404be1303da3a1b00f163b64fde4f6
Baliasnyi Serghii <sergey070373@gmail.com>
2022-12-04 20:57:48 +0000

    logout

--- src/main/java/local/vtos2/config/SecurityConfig.java
+++ src/main/java/local/vtos2/config/SecurityConfig.java
@@ -42,7 +42,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .formLogin().loginPage("/auth/login")
                         .loginProcessingUrl("/process_login")
                         .defaultSuccessUrl("/",true)
-                        .failureUrl("/auth/login?error");
+                        .failureUrl("/auth/login?error")
+                        .and()
+                        .logout()
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/auth/login");
     }
 
 
